1973f69f32065fd1dcc3ad82534ed37bc5977c44
Laila <lailacampbell96@icloud.com>
2021-04-13 02:44:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 puts "R: #{colors[0]} red, O: #{colors[1]} orange, Y: #{colors[2]} yellow, G: #{colors[3]} green, B: #{colors[4]} blue, I: #{colors[5]} indigo, V: #{colors[6]} violet"
 end
-#display_rainbow
+display_rainbow
